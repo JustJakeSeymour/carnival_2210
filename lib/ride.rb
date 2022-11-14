6d8ed545b0ce45f544ebdac57b06dd8f5d1623f2
J Seymour <JustJakeSeymour@gmail.com>
2022-11-14 09:40:15 -0700

    pass first ride test

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,20 @@
 class Ride
+  attr_reader :name,
+  :min_height,
+  :admission_fee,
+  :excitement,
+  :rider_log
+
+  def initialize(hash)
+    @name = hash[:name]
+    @min_height = hash[:min_height]
+    @admission_fee = hash[:admission_fee]
+    @excitement = hash[:excitement]
+    @rider_log = {}
+  end
+
+  def total_revenue
+    0
+  end
 
 end
--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -2,12 +2,10 @@ require './lib/ride'
 require './lib/visitor'
 
 RSpec.describe Ride do
-  let!(ride1) {Ride.new({ :name 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })}
-  let!(visitor1) {Visitor.new('Bruce', 54, '$10')}
-  let!(visitor2) {Visitor.new('Tucker', 36, '$5')}
-  let!(visitor1.add_preference(:gentle))
-  let!(visitor2.add_preference(:gentle))
-
+  let!(:ride1) {Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })}
+  let!(:visitor1) {Visitor.new('Bruce', 54, '$10')}
+  let!(:visitor2) {Visitor.new('Tucker', 36, '$5')}
+  
   it 'is created as a Ride class with attributes supplied in a single hash' do
     expect(ride1).to be_an_instance_of Ride
     expect(ride1.name).to eq 'Carousel'
@@ -15,10 +13,12 @@ RSpec.describe Ride do
     expect(ride1.admission_fee).to eq 1
     expect(ride1.excitement).to eq :gentle
     expect(ride1.total_revenue).to eq 0
-    expect(ride1.rider_log).to eq {}
+    expect(ride1.rider_log).to eq ({})
   end
   
   it 'boards riders and adds them to the rider log' do
+    visitor1.add_preference(:gentle)
+    visitor2.add_preference(:gentle)
     expect(ride1.rider_log).to eq {}
     ride1.board_rider(visitor1)
     expect(ride1.rider_log).to eq ({ visitor1: 1 })
@@ -29,9 +29,11 @@ RSpec.describe Ride do
   end
   
   it 'removes cost of ride from visitor for each time they ride' do
+    visitor1.add_preference(:gentle)
+    visitor2.add_preference(:gentle)
     expect(visitor1.spending_money).to eq 10
     expect(visitor2.spending_money).to eq 5
-
+    
     ride1.board_rider(visitor1)
     ride1.board_rider(visitor2)
     
@@ -44,6 +46,8 @@ RSpec.describe Ride do
   end
   
   it 'knows the revenue based on amount of rides taken' do
+    visitor1.add_preference(:gentle)
+    visitor2.add_preference(:gentle)
     expect(ride1.total_revenue).to eq 0
     
     ride1.board_rider(visitor1)
@@ -61,6 +65,8 @@ RSpec.describe Ride do
     ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
     ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
     
+    visitor1.add_preference(:gentle)
+    visitor2.add_preference(:gentle)
     visitor2.add_preference(:thrilling)
     visitor3.add_preference(:thrilling)
     
@@ -76,10 +82,10 @@ RSpec.describe Ride do
     expect(visitor2.spending_money).to eq 5
     expect(visitor3.spending_money).to eq 13
     
-    expect(ride3.ride_log).to eq { visitor3: 1 }
+    expect(ride3.ride_log).to eq ({ visitor3: 1 })
     expect(ride3.total_revenue).to eq 2
   end
 
-  
+
   
 end
