bd4c53e0b621fa155f062e82e6ec72b940857a6c
J Seymour <JustJakeSeymour@gmail.com>
2022-11-14 09:33:52 -0700

    complete writing iteration 2 tests

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -55,4 +55,31 @@ RSpec.describe Ride do
     ride1.board_rider(visitor1)
     expect(ride1.total_revenue).to eq 3
   end
+  
+  it 'does not let visitors ride if they do not reach the minimum height or do not like the excitement level' do
+    visitor3 = Visitor.new('Penny', 64, '$15')
+    ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+    ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
+    
+    visitor2.add_preference(:thrilling)
+    visitor3.add_preference(:thrilling)
+    
+    expect(visitor1.spending_money).to eq 10
+    expect(visitor2.spending_money).to eq 5
+    expect(visitor3.spending_money).to eq 15
+    
+    ride3.board_rider(visitor1)
+    ride3.board_rider(visitor2)
+    ride3.board_rider(visitor3)
+    
+    expect(visitor1.spending_money).to eq 10
+    expect(visitor2.spending_money).to eq 5
+    expect(visitor3.spending_money).to eq 13
+    
+    expect(ride3.ride_log).to eq { visitor3: 1 }
+    expect(ride3.total_revenue).to eq 2
+  end
+
+  
+  
 end
