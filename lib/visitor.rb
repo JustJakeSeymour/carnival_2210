e0ed3f92bc2a14ef6178083f9f5b3fd533524f12
J Seymour <JustJakeSeymour@gmail.com>
2022-11-14 09:12:27 -0700

    pass first visitor test

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,3 +1,17 @@
 class Visitor
+  attr_reader :name,
+              :height,
+              :preferences
 
+  def initialize(name, height, spending_money)
+    @name = name
+    @height = height
+    @spending_money = spending_money
+    @preferences = []
+  end
+
+  def spending_money
+    @spending_money.tr("$","").to_i
+  end
+  
 end
